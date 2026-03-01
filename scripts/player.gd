19fd2c54556a7fda8fda73e00e52ea2c633c560c
thenightness <147348310+thenightness@users.noreply.github.com>
2026-03-01 17:06:06 +0100

    change jump speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 
-const movementSpeed = 130.0
+const walkSpeed = 100.0
+const pushSpeed = 200.0
 const jump_velocity = -300.0
-const wall_jump_push = 300.0
 
 
 func _physics_process(delta: float) -> void:
 	var wall_normal = get_wall_normal()
 	var input_direction := Input.get_axis("ui_left", "ui_right")
-	# 1. Standard-Schwerkraft anwenden, wenn man nicht am Boden ist
+	# 1. Luft
 	if not is_on_floor():
 		# 2. Wall-Hang
 		# Prüfen: Berühren wir eine Wand, sind in der Luft und halten "Jump"?
@@ -23,24 +23,26 @@ func _physics_process(delta: float) -> void:
 			# wall_normal.x ist 1 (Wand links) oder -1 (Wand rechts)
 			if (wall_normal.x > 0 and input_direction > 0) or (wall_normal.x < 0 and input_direction < 0):
 				# Wir geben dem Spieler einen Kick in beide Achsen
-				velocity.x = wall_normal.x * wall_jump_push
+				velocity.x = wall_normal.x * pushSpeed
 				velocity.y = jump_velocity
-			
 			#! Optional: Hier eine "Hang"-Animation abspielen
-		
 		else:
 			velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_velocity
-		
-	# Get the input input_direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	if input_direction:
-		velocity.x = input_direction * movementSpeed
+		velocity.x = input_direction * pushSpeed
+	elif not is_on_floor():
+		# Get the input input_direction and handle the movement/deceleration.
+		if input_direction:
+			velocity.x = input_direction * pushSpeed
+		else:
+			velocity.x = move_toward(velocity.x, 0, pushSpeed)
 	else:
-		velocity.x = move_toward(velocity.x, 0, movementSpeed)
+		# Get the input input_direction and handle the movement/deceleration.
+		if input_direction:
+			velocity.x = input_direction * walkSpeed
+		else:
+			velocity.x = move_toward(velocity.x, 0, walkSpeed)
 
 	move_and_slide()
